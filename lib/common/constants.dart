520d0bd4a83b417f9ac607da722cf4d0239174ee
Jimit Raval <12jraval@gmail.com>
2023-01-16 20:42:27 +0530

    NotesApp-flutter: pubspec.yaml: Updated, constants, string_values, theme: code added.

---
+++ lib/common/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFA94CE8);
+const kAccentColor = Color(0xFF5CA1FE);
+const kSecondaryColor = Color(0xFFFD5C8F);
+const kSecondaryDark = Color(0xff000000);
+const kScaffoldDark = Color(0xFF2D1D31);
+const kTextColor = Color(0xFF646464);
+const kBorderColor = Color(0xFF757575);
+const kAuthScreenColor1 = Color(0xFF2A4C4D);
+const kAuthScreenColor2 = Color(0xFF155CDE);
+const kGlobalOuterPadding = EdgeInsets.all(10.0);
+const kGlobalCardPadding = EdgeInsets.all(5.0);
+const kGlobalTextPadding =
+EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0);
+
+const kBaseUrl = '';
+
+const kAppName = 'NotesApp-flutter';
+
+const kVSpace = SizedBox(
+  height: 15.0,
+);
+const kHSpace = SizedBox(
+  width: 10.0,
+);
